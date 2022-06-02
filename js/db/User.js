34b7c3df3e7593ba80426dcdde09508fe1ebae87
Veronica <veronica.stevania@gmail.com>
2022-06-02 02:27:12 +0200

    exercice Singleton Pattern

--- js/db/User.js
+++ js/db/User.js
@@ -1,3 +1,47 @@
 class User {
-    
+    constructor(data) {
+        if (User.exists) {
+            return User.instance
+        } else if (data && data.firstName && data.lastName) {
+            this._fisrtName = data.firstName
+            this._lastName = data.lastName 
+
+            this.saveLocalStorage ()
+            User.instance = this
+            User.exists = true
+
+            return this
+        }
+    }
+
+    get firstName() {
+        return this._fisrtName
+    }
+    get lastName() {
+        return this._lastName
+    }
+    get user () {
+        const firstName = this._firstName || localStorage.getItem('firstName')
+        const lastName = this._lastName || localStorage.getItem('lastName')
+
+        if (firstName && lastName) {
+            const user = new User({
+                firstName,
+                lastName
+            })
+        }
+
+        if (!firstName && !lastName) {
+            return null
+        }
+        return {
+            firstName: firstName,
+            lastName: lastName
+        }
+    }
+
+    saveLocalStorage() {
+        localStorage.setItem('firstName', this._fisrtName)
+        localStorage.setItem('lastName', this._lastName)
+    }
 }
